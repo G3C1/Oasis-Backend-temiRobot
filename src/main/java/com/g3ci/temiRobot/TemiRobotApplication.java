34061fbe034eb3409f183e6dd3d78431e950e143
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-10-21 09:05:47 +0900

    :tada: 프젝시작
    ㅇㅇ

---
+++ src/main/java/com/g3ci/temiRobot/TemiRobotApplication.java
@@ -0,0 +1,13 @@
+package com.g3ci.temiRobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemiRobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemiRobotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/g3ci/temiRobot/TemiRobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g3ci.temiRobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemiRobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
